49d84f429e25e132d26f1564479be105b2e13752
James Jahraus <james.jahraus@gmail.com>
2019-01-16 22:26:39 -0700

    Update main.rs
    Added cli version of gcd.

--- programming-rust/chapter-02/gcd/src/main.rs
+++ programming-rust/chapter-02/gcd/src/main.rs
@@ -18,6 +18,32 @@ fn test_gcd() {
     assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
 
+/// The use declarations birng the two _traits_ Write and FromStr into scope.
+/// A trait is a collection of methods that types can implement.
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    /// Declare a mutable local var numbers, and initialize it to an empty vector.
+    /// Vec is like a python list.
+    let mut numbers = Vec::new();
+
+    /// std::env::args returns an iterator
+    /// The first values produced by the iterator returned by std::env::args
+    /// is the name of the program being run so we skip(1) it.
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
